4454cf01684645a9834eeed934dee52437d79ec8
hcliao <Liao.elton@gmail.com>
2022-11-30 01:07:29 +0800

    modify run npc

--- run_npc.sh
+++ run_npc.sh
@@ -2,6 +2,47 @@
 # for num_parameters in {6,12,25,50,100}
 EXP_NAME=onlyFIConditionalNotStop
 echo $EXP_NAME
+
+
+for num_parameters in {100,250,500,750}
+do
+    if [ $num_parameters -eq 100 ]; then
+        limit=10000000
+        interval=500000
+    fi
+    if [ $num_parameters -eq 250 ]; then
+        limit=10000000
+        interval=1000000
+    fi
+    if [ $num_parameters -eq 500 ]; then
+        limit=15000000
+        interval=1500000
+    fi
+    if [ $num_parameters -eq 750 ]; then
+        limit=20000000
+        interval=2000000
+    fi
+    for i in {1..30}
+    do
+        echo ./MO_GOMEA -r 2 2 $num_parameters 2000 $limit $interval $i &
+        mkdir -p knapsack${num_parameters}_result/${EXP_NAME}/result$i &
+        # Standard version, no mutation, no stopping small population
+        ./MO_GOMEA -r 2 2 $num_parameters 2000 $limit $interval $i 0 ${EXP_NAME} | tee knapsack${num_parameters}_result/${EXP_NAME}/result$i/log.txt &
+        
+        # With weak mutation
+        #./MO_GOMEA -r -m 2 2 $num_parameters 2000 $limit $interval
+        # With strong mutation
+        #./MO_GOMEA -r -M 2 2 $num_parameters 2000 $limit $interval
+        # With stopping small populations
+        #./MO_GOMEA -r -z 2 2 $num_parameters 2000 $limit $interval
+        # for f in elitist*.dat;
+        # 	do mv $f ${num_parameters}_${i}_${f};
+        # done
+    done
+    wait
+    echo All $num_parameters knapsack tasks finished
+done
+
 for num_parameters in {6,25,50,100,200,400}
 do
 	if [ $num_parameters -eq 6 ]; then
@@ -51,42 +92,3 @@ do
 	echo All $num_parameters maxcut tasks finished
 done
 
-for num_parameters in {100,250,500,750}
-do
-    if [ $num_parameters -eq 100 ]; then
-        limit=10000000
-        interval=500000
-    fi
-    if [ $num_parameters -eq 250 ]; then
-        limit=10000000
-        interval=1000000
-    fi
-    if [ $num_parameters -eq 500 ]; then
-        limit=15000000
-        interval=1500000
-    fi
-    if [ $num_parameters -eq 750 ]; then
-        limit=20000000
-        interval=2000000
-    fi
-    for i in {1..30}
-    do
-        echo ./MO_GOMEA -r 2 2 $num_parameters 2000 $limit $interval $i &
-		mkdir -p maxcut${num_parameters}_result/${EXP_NAME}/result$i &
-        mkdir -p knapsack${num_parameters}_result/${EXP_NAME}/result$i &
-        # Standard version, no mutation, no stopping small population
-        ./MO_GOMEA -r 2 2 $num_parameters 2000 $limit $interval $i 0 &
-        
-        # With weak mutation
-        #./MO_GOMEA -r -m 2 2 $num_parameters 2000 $limit $interval
-        # With strong mutation
-        #./MO_GOMEA -r -M 2 2 $num_parameters 2000 $limit $interval
-        # With stopping small populations
-        #./MO_GOMEA -r -z 2 2 $num_parameters 2000 $limit $interval
-        # for f in elitist*.dat;
-        # 	do mv $f ${num_parameters}_${i}_${f};
-        # done
-    done
-    wait
-    echo All $num_parameters knapsack tasks finished
-done
